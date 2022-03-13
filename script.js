9f0164963abddf0e403842ba01da1fd58c42d3b1
Chaithu1216 <99240181+Chaithu1216@users.noreply.github.com>
2022-03-13 23:07:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var r
+let aoc=0,coc=0
+const pi=3.14
+r=3
+aoc=pi*r*r;
+area=pi*r*r;
+coc=2*pi*r;
+console.log("Radius of Circle = "+ r+"cm")
+console.log("Area of Circle = "+aoc+"cm2");
+console.log("Circumference of Circle = "+coc+"cm");
